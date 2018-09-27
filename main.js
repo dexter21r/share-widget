c4243019355437b339eb0a8b5a2c523822b8897f
Ranjeet Rajput <dexter21r@gmail.com>
2018-09-27 21:37:40 +0530

    Canvas BG color functionality added, code separated into different files

---
+++ main.js
@@ -0,0 +1,376 @@
+var colorPicker = null;
+
+$(".colorPickSelector").colorPick({
+    'initialColor': '#27ae60',
+    'onColorSelected': function () {
+        colorPicker = this;
+        console.log("The user has selected the color: " + this.color)
+        this.element.css({
+            'backgroundColor': this.color,
+            'color': this.color
+        });
+        applyStyle('font-color', this.color)
+    }
+});
+
+var textAreaStyle = {
+    fontSize: 11,
+    lineHeight: 1,
+    fontFamily: 'Wonderbar',
+    textAlign: 'left',
+    fontWeight: "",
+    textDecoration: "",
+    fontStyle: "",
+    fill: '#00000',
+    charSpacing: 1
+};
+
+var styleObj = {
+    'border-color': 'borderColor',
+    'font-size': 'fontSize',
+    'font-family': 'fontFamily',
+    'text-align': 'textAlign',
+    'font-style': 'fontStyle',
+    'letter-spacing': 'charSpacing',
+    'line-height': 'lineHeight',
+    'bg-color': 'textBackgroundColor',
+    'font-color': 'fill',
+    'font-weight': 'fontWeight',
+    'text-decoration': 'textDecoration'
+};
+
+var canvas;
+var scaleX, scaleY, textArea, textArea2, brand;
+var totalAssets = 3;
+var loadedAssets = 0;
+var canvasScale = 1;
+var SCALE_FACTOR = 1.2;
+var backgroundImage = '';
+var panning = false;
+var orientation = 'horizontal';
+
+//$('.shareit').click(function () {
+console.log('Share Button Clicked');
+$('#tb-share-dialog').show();
+//});
+
+$(document).on('click', '.tb-share-dialog-close', function () {
+    $('#tb-share-dialog').hide();
+});
+
+// button Zoom In
+$("#btnZoomIn").click(function () {
+    zoomIn();
+});
+// button Zoom Out
+$("#btnZoomOut").click(function () {
+    zoomOut();
+});
+// button Reset Zoom
+$("#btnResetZoom").click(function () {
+    resetZoom();
+});
+
+$(document).on('click', '.tb-search-items', function () {
+    console.log('image clicked');
+    var imageUrl = $(this).data('url');
+    $.ajax({
+        url: 'tb-share-widget.php',
+        type: 'POST',
+        data: {
+            action: 'save-image',
+            imageurl: imageUrl
+        },
+        success: function (res) {
+            console.log(res);
+            fabric.Image.fromURL(res.data, function (img) {
+                backgroundImage = img;
+                canvas.setBackgroundImage(backgroundImage, canvas.renderAll.bind(
+                    canvas), {
+                    scaleX: canvas.width / backgroundImage.width,
+                    scaleY: canvas.height / backgroundImage.height
+                });
+            });
+
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+});
+
+$(document).on('click', '#tb-btnsearch', function () {
+    var searchTerm = $('#tb-txtsearch').val();
+    console.log('Search', searchTerm);
+
+    if (searchTerm && searchTerm.length > 3) {
+        console.log('Search it');
+        $.ajax({
+            url: 'tb-share-widget.php',
+            type: 'POST',
+            data: {
+                action: 'search',
+                searchterm: searchTerm,
+                orientation: orientation
+            },
+            success: function (res) {
+                console.log(res);
+                var images = res.data;
+                var searchHtml = '';
+                images.forEach(function (item) {
+                    searchHtml +=
+                        '<div class="tb-search-items" data-url="' + item.largeImageURL +
+                        '">\n\
+                                    <a target="_blank" href="' +
+                        item.pageURL +
+                        '" class="tb-search-image-source-link">\n\
+                                    <img class="tb-search-image-source"  src="pixabay.svg" /></a>\n\
+                                    <img class="tb-search-image-preview" src="' +
+                        item.previewURL +
+                        '" />\n\
+                                </div>';
+                })
+                $('#tb-bg-search-results').html(searchHtml);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+});
+
+$(document).on('click', '.tb-share-platform-icon', function () {
+    console.log($(this).data('platform'));
+    $('#tb-share-platform-card').hide();
+    $('#tb-share-canvas-card').show();
+
+    var width = 1024;
+    var height = 1024;
+    orientation = 'horizontal';
+    var platform = $(this).data('platform');
+    switch (platform) {
+        case 'fb':
+            width = 940;
+            height = 788;
+            break;
+        case 'in':
+            width = 1080;
+            height = 1080;
+            break;
+        case 'tw':
+            width = 1024;
+            height = 512;
+            break;
+        case 'pi':
+            width = 600;
+            height = 900;
+            orientation = 'vertical';
+            break;
+        case 'gp':
+            width = 1024;
+            height = 512;
+            break;
+        case 'ge':
+            width = 1024;
+            height = 1024;
+            break;
+        default:
+            break;
+    }
+
+    totalAssets = 3;
+    loadedAssets = 0;
+
+    canvas = new fabric.Canvas('tb-share-canvas', {});
+
+    canvas.setWidth(width);
+    canvas.setHeight(height);
+
+    // initCenteringGuidelines(canvas);
+
+    var deviceHeight = 600 * 70 / 100; //480; //;262.5;
+    var deviceWidth = width * deviceHeight / height;
+
+    $('.tb-canvas-scroll').height(deviceHeight + 10);
+    console.log('Width: ' + deviceWidth);
+    console.log('Height: ' + deviceHeight);
+    scaleX = (deviceWidth / width);
+    scaleY = (deviceHeight / height);
+    console.log('Scale Factor: X: ' + scaleX + ' , Y: ' + scaleY);
+
+
+    fabric.Image.fromURL(
+        'http://www.qygjxz.com/data/out/129/4777797-images-wallpaper.jpeg',
+        function (img) {
+            backgroundImage = img;
+            console.log('Image width: ' + (width / img.width) + 'Image height: ' + img.height);
+            // var oImg = backgroundImage.set({
+            //     // left: 0,
+            //     // top: 0,
+            //     // angle: 0,
+            //     // width: width,
+            //     // height: height,
+            //     // scaleX: width / img.width, // Mobileview
+            //     // scaleY:  height / img.height// Mobileview
+            // });
+            canvas.setBackgroundImage(backgroundImage, canvas.renderAll.bind(canvas), {
+                scaleX: canvas.width / backgroundImage.width,
+                scaleY: canvas.height / backgroundImage.height
+            });
+
+            //canvas.backgroundImageStretch = true;
+            //canvas.renderAll();
+            // canvas.backgroundImageStretch = true;
+            console.log('==> backgroud loaded');
+            // var dataURL = canvas.toDataURL({
+            // 	format: 'png',
+            // 	quality: 0.8
+            // });
+            loadedAssets++;
+            checkAssetsLoading();
+        });
+
+    // Add Brand Name 
+    brand = new fabric.Text('site.com', {
+        left: canvas.getWidth() - 160,
+        top: 5,
+        fontSize: 21,
+        fontFamily: 'Josefin Slab',
+        textAlign: 'center',
+        fontWeight: "bold",
+        fill: '#fff',
+        borderColor: "#CCC",
+        selectable: false,
+        stroke: 'rgba(255, 255, 255,0.5)',
+        strokeWidth: 0.2,
+        // lineHeight: ta.defaultSettings.lineHeight,
+        // fontFamily: ta.defaultSettings.fontFamily,
+    });
+
+    canvas.add(brand);
+    loadedAssets++;
+    checkAssetsLoading();
+    // ------
+
+    textArea = new fabric.Textbox(
+        'To accomplish great things, we must not only act, but also dream; not only plan, but also believe.', {
+            width: canvas.getWidth() - 200,
+            // stroke: 'rgba(0,0,0,0.5)',
+            // strokeWidth: 0.2,
+            fontSize: 48,
+            fontFamily: 'Lobster Two',
+            textAlign: 'center',
+            // fontWeight: "bold",
+            textDecoration: "normal",
+            fontStyle: "normal",
+            fill: 'yellow',
+            borderColor: "#CCC",
+            editable: false,
+            // lineHeight: ta.defaultSettings.lineHeight,
+            // fontFamily: ta.defaultSettings.fontFamily,
+        });
+    textArea._forceClearCache = true;
+
+    textArea.set({
+        left: (canvas.getWidth() / 2) - (textArea.width / 2),
+        top: ((canvas.getHeight() / 2) - (textArea.height / 2)),
+        borderDashArray: [2, 2],
+        cornerColor: 'white',
+        cornerStrokeColor: 'black',
+        cornerStyle: 'circle',
+        cornerSize: 12,
+        padding: 15,
+        transparentCorners: false,
+        borderScaleFactor: 2
+    });
+
+    textArea.setControlsVisibility({
+        mt: false,
+        mb: false,
+    });
+
+    textArea.on("changed", function (e) {
+        //console.log('textchanges',e);
+    });
+
+    canvas.add(textArea);
+    loadedAssets++;
+    checkAssetsLoading();
+
+    textArea2 = new fabric.Textbox('- Author Name', {
+        width: 200,
+        height: 150,
+        stroke: 'rgba(0,0,0,0.5)',
+        strokeWidth: 0.2,
+        fontSize: 22,
+        fontFamily: 'Bitter',
+        textAlign: 'center',
+        // fontWeight: "bold",
+        textDecoration: "normal",
+        fontStyle: "normal",
+        fill: '#fff',
+        borderColor: "#CCC",
+        editable: false,
+        // lineHeight: ta.defaultSettings.lineHeight,
+        // fontFamily: ta.defaultSettings.fontFamily,
+    });
+    textArea2._forceClearCache = true;
+
+    textArea2.set({
+        left: (canvas.getWidth() / 2) - (textArea2.width / 2),
+        top: textArea.top + textArea.height + 50,
+        borderDashArray: [2, 2],
+        cornerColor: 'white',
+        cornerStrokeColor: 'black',
+        cornerStyle: 'circle',
+        cornerSize: 12,
+        padding: 15,
+        transparentCorners: false,
+        borderScaleFactor: 2
+    });
+
+    textArea2.on("changed", function (e) {
+        //console.log('textchanges',e);
+    });
+
+    canvas.add(textArea2);
+    loadedAssets++;
+    checkAssetsLoading();
+
+    canvas.on("mouse:up", function (e) {
+        //console.log(e);
+        if (e.target != null) {
+            var type = e.target.get('type');
+            if (type == 'textbox') {
+                console.log('textbox');
+                $('#tb-share-text-controls').show();
+                textAreaStyle = {
+                    fontSize: e.target.fontSize,
+                    lineHeight: e.target.lineHeight,
+                    fontFamily: e.target.fontFamily,
+                    textAlign: e.target.textAlign,
+                    fontWeight: e.target.fontWeight !== undefined ? e.target.fontWeight : "normal",
+                    textDecoration: e.target.textDecoration,
+                    fontStyle: e.target.fontStyle !== undefined ? e.target.fontStyle : "normal",
+                    fill: e.target.fill,
+                    charSpacing: e.target.charSpacing !== undefined ? e.target.charSpacing : 1
+                };
+
+                setFormattingValues();
+            } else if (type == 'path-group') {
+
+            }
+
+        } else {
+            $('#tb-share-text-controls').hide();
+        }
+    });
+
+});
+
+$(document).on('click', '.tb-share-dialog-back', function () {
+    $('#tb-share-platform-card').show();
+    $('#tb-share-canvas-card').hide();
+    canvas.dispose();
+
+});
---
+++ utils.js
@@ -0,0 +1,330 @@
+function resizeCanvas() {
+    var w = canvas.getWidth() * scaleX;
+    var h = canvas.getHeight() * scaleY;
+    canvas.setWidth(w);
+    canvas.setHeight(h);
+    console.log(w + ' ' + h + ' ' + canvas.getWidth() + ' ');
+    $('.tb-share-canvas-container').css({
+        width: w + 5,
+        height: h + 5
+    });
+
+    var objects = canvas.getObjects();
+    for (var i in objects) {
+
+        console.log('=========change size', objects[i].get('type'));
+        var sX = objects[i].scaleX;
+        var sY = objects[i].scaleY;
+        var left = objects[i].left;
+        var top = objects[i].top;
+
+        var tempScaleX = sX * scaleX;
+        var tempScaleY = sY * scaleY;
+        var tempLeft = left * scaleX;
+        var tempTop = top * scaleY;
+
+        objects[i].scaleX = tempScaleX;
+        objects[i].scaleY = tempScaleY;
+        objects[i].left = tempLeft;
+        objects[i].top = tempTop;
+
+        objects[i].setCoords();
+    }
+
+    canvas.renderAll();
+}
+
+function checkAssetsLoading() {
+    if (totalAssets == loadedAssets) {
+        resizeCanvas();
+    }
+}
+
+// Zoom In
+function zoomIn() {
+    // TODO limit the max canvas zoom in
+
+    canvasScale = canvasScale * SCALE_FACTOR;
+
+    canvas.setHeight(canvas.getHeight() * SCALE_FACTOR);
+    canvas.setWidth(canvas.getWidth() * SCALE_FACTOR);
+
+    var objects = canvas.getObjects();
+    for (var i in objects) {
+        var scaleX = objects[i].scaleX;
+        var scaleY = objects[i].scaleY;
+        var left = objects[i].left;
+        var top = objects[i].top;
+
+        var tempScaleX = scaleX * SCALE_FACTOR;
+        var tempScaleY = scaleY * SCALE_FACTOR;
+        var tempLeft = left * SCALE_FACTOR;
+        var tempTop = top * SCALE_FACTOR;
+
+        objects[i].scaleX = tempScaleX;
+        objects[i].scaleY = tempScaleY;
+        objects[i].left = tempLeft;
+        objects[i].top = tempTop;
+
+        objects[i].setCoords();
+    }
+
+    var oImg = backgroundImage.set({
+        scaleX: backgroundImage.scaleX * SCALE_FACTOR,
+        scaleY: backgroundImage.scaleY * SCALE_FACTOR
+    });
+    canvas.setBackgroundImage(oImg);
+
+    $('.tb-share-canvas-container').css({
+        width: canvas.getWidth() + 5,
+        height: canvas.getHeight() + 5
+    });
+
+    canvas.renderAll();
+}
+
+// Zoom Out
+function zoomOut() {
+    // TODO limit max cavas zoom out
+
+    canvasScale = canvasScale / SCALE_FACTOR;
+
+    canvas.setHeight(canvas.getHeight() * (1 / SCALE_FACTOR));
+    canvas.setWidth(canvas.getWidth() * (1 / SCALE_FACTOR));
+
+    var objects = canvas.getObjects();
+    for (var i in objects) {
+        var scaleX = objects[i].scaleX;
+        var scaleY = objects[i].scaleY;
+        var left = objects[i].left;
+        var top = objects[i].top;
+
+        var tempScaleX = scaleX * (1 / SCALE_FACTOR);
+        var tempScaleY = scaleY * (1 / SCALE_FACTOR);
+        var tempLeft = left * (1 / SCALE_FACTOR);
+        var tempTop = top * (1 / SCALE_FACTOR);
+
+        objects[i].scaleX = tempScaleX;
+        objects[i].scaleY = tempScaleY;
+        objects[i].left = tempLeft;
+        objects[i].top = tempTop;
+
+        objects[i].setCoords();
+    }
+
+    var oImg = backgroundImage.set({
+        scaleX: backgroundImage.scaleX / SCALE_FACTOR,
+        scaleY: backgroundImage.scaleY / SCALE_FACTOR
+    });
+    canvas.setBackgroundImage(oImg);
+
+    $('.tb-share-canvas-container').css({
+        width: canvas.getWidth() + 5,
+        height: canvas.getHeight() + 5
+    });
+
+    canvas.renderAll();
+}
+
+// Reset Zoom
+function resetZoom() {
+
+    canvas.setHeight(canvas.getHeight() * (1 / canvasScale));
+    canvas.setWidth(canvas.getWidth() * (1 / canvasScale));
+
+    var objects = canvas.getObjects();
+    for (var i in objects) {
+        var scaleX = objects[i].scaleX;
+        var scaleY = objects[i].scaleY;
+        var left = objects[i].left;
+        var top = objects[i].top;
+
+        var tempScaleX = scaleX * (1 / canvasScale);
+        var tempScaleY = scaleY * (1 / canvasScale);
+        var tempLeft = left * (1 / canvasScale);
+        var tempTop = top * (1 / canvasScale);
+
+        objects[i].scaleX = tempScaleX;
+        objects[i].scaleY = tempScaleY;
+        objects[i].left = tempLeft;
+        objects[i].top = tempTop;
+
+        objects[i].setCoords();
+    }
+
+    var oImg = backgroundImage.set({
+        scaleX: backgroundImage.scaleX * (1 / canvasScale),
+        scaleY: backgroundImage.scaleY * (1 / canvasScale)
+    });
+    canvas.setBackgroundImage(oImg);
+
+    $('.tb-share-canvas-container').css({
+        width: canvas.getWidth() + 5,
+        height: canvas.getHeight() + 5
+    });
+
+    canvas.renderAll();
+
+    canvasScale = 1;
+}
+
+
+
+function setFormattingValues() {
+
+    $('#tb-color-pallate').val(textAreaStyle.fontFamily);
+    $('#tb-font-size').val(textAreaStyle.fontSize);
+
+    if (textAreaStyle.fontWeight === 'bold') {
+        $('#txt-bold').addClass('active');
+    } else {
+        $('#txt-bold').removeClass('active');
+    }
+
+    if (textAreaStyle.fontStyle === 'italic') {
+        $('#txt-italic').addClass('active');
+    } else {
+        $('#txt-italic').removeClass('active');
+    }
+
+    if (textAreaStyle.textDecoration === 'underline') {
+        $('#txt-underline').addClass('active');
+    } else {
+        $('#txt-underline').removeClass('active');
+    }
+
+    if (textAreaStyle.textAlign === 'left') {
+        $('#txt-align-left').show();
+        $('#txt-align-center').hide();
+        $('#txt-align-right').hide();
+    } else if (textAreaStyle.textAlign === 'center') {
+        $('#txt-align-left').hide();
+        $('#txt-align-center').show();
+        $('#txt-align-right').hide();
+    } else if (textAreaStyle.textAlign === 'right') {
+        $('#txt-align-left').hide();
+        $('#txt-align-center').hide();
+        $('#txt-align-right').show();
+    }
+
+    colorPicker.color = textAreaStyle.fill;
+    colorPicker.element.val(colorPicker.color);
+    colorPicker.element.css({'backgroundColor': colorPicker.color, 'color': colorPicker.color});
+    console.log(colorPicker);
+    //$.proxy(colorPicker.options.onColorSelected, colorPicker)();
+
+    // var col = textAreaStyle.fill;
+    // var colName = getColorNameFromCode(col);
+    // pk.setColor(col, colName);
+    // wrapperEl.style.backgroundColor = col;
+
+    // col = textAreaStyle.borderColor;
+    // colName = getColorNameFromCode(col);
+    // pkBorder.setColor(col, colName);
+    // wrapperElBorder.style.backgroundColor = col;
+
+}
+
+function applyStyle(styleName, styleValue, styleType) {
+    var obj;
+    if (obj = getActiveObject()) {
+        //console.log('Apply Style',obj.getSelectionStyles(),obj.selectionStart ,obj.selectionEnd,obj );
+        styleName = styleObj[styleName];
+
+        if (styleName != undefined) {
+            console.log(styleName + ' ==> ' + styleValue);
+            if (styleType === 'selection') {
+                var isAlreadyApplied = (getStyle(obj, styleName) || '').indexOf(styleValue) > -1;
+                styleValue = isAlreadyApplied ? '' : styleValue;
+                setStyle(obj, styleName, styleValue);
+            } else {
+                setStyle(obj, styleName, styleValue);
+            }
+            setActiveStyle(styleName, styleValue);
+            refresh();
+        } else {
+            console.log('Invalid Style: ' + styleName);
+        }
+
+
+    } else {
+        console.log('Object not selected');
+    }
+}
+
+function setActiveStyle(styleName, styleValue) {
+
+    switch (styleName) {
+        case 'fontSize':
+            this.textAreaStyle.fontSize = styleValue;
+            break;
+        case 'fontFamily':
+            this.textAreaStyle.fontFamily = styleValue;
+            break;
+        case 'textAlign':
+            this.textAreaStyle.textAlign = styleValue;
+            break;
+        case 'fontStyle':
+            this.textAreaStyle.fontStyle = styleValue;
+            break;
+        case 'charSpacing':
+            this.textAreaStyle.charSpacing = styleValue;
+            break;
+        case 'lineHeight':
+            this.textAreaStyle.lineHeight = styleValue;
+            break;
+        case 'borderColor':
+            this.textAreaStyle.borderColor = styleValue;
+            break;
+        case 'fill':
+            this.textAreaStyle.fill = styleValue;
+            break;
+        case 'fontWeight':
+            this.textAreaStyle.fontWeight = styleValue;
+            break;
+        case 'textDecoration':
+            this.textAreaStyle.textDecoration = styleValue;
+            break;
+        default:
+            console.log('Sorry, we are out of ' + styleName + '.');
+    }
+
+    setFormattingValues();
+}
+
+function getActiveObject() {
+    if (canvas) {
+        return canvas.getActiveObject();
+    }
+    return false;
+}
+
+function setStyle(object, styleName, value) {
+
+    if (object.setSelectionStyles && object.isEditing) {
+        var style = {};
+        style[styleName] = value;
+        object.setSelectionStyles(style);
+    } else {
+        if (styleName == 'fill') {
+            object.setColor(value);
+        } else {
+            object[styleName] = value;
+        }
+        object.set({
+            dirty: true
+        });
+    }
+}
+
+function getStyle(object, styleName) {
+    return (object.getSelectionStyles && object.isEditing) ?
+        object.getSelectionStyles()[styleName] :
+        object[styleName];
+}
+
+function refresh() {
+    console.log('template updated');
+    canvas.renderAll();
+    canvas.renderAll.bind(canvas);
+}
